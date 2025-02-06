236f37d8b7a6cff4ea6e52232668dfb538a023fb
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-02-06 09:07:04 +0900

    Add files via upload

---
+++ 2주차/1987. 알파벳/김소중.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Main_1987 {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int[][] dels = {{-1,0},{1,0},{0,-1},{0,1}};
+	boolean[] visited = new boolean[26];
+	char[][] arr;
+	int h, w, maxCount;
+	
+	public void init() throws IOException {
+		String[] inpuStrs = br.readLine().split(" ");
+		h = Integer.parseInt(inpuStrs[0]);
+		w = Integer.parseInt(inpuStrs[1]);
+		
+		arr = new char[h][w];
+		for (int i = 0; i < h; i++) {
+			arr[i] = br.readLine().toCharArray();
+		}
+		dfs(0, 0, 1);
+	}
+	
+	public void dfs(int y, int x, int count) {
+		visited[arr[y][x]-'A'] = true;
+		
+		boolean isEnd = true;
+		for (int[] del : dels) {
+			int ny = y + del[0];
+			int nx = x + del[1];
+			if (isIn(ny, nx) && !visited[arr[ny][nx]-'A']) {
+				isEnd = false;
+				dfs(ny, nx, count+1);
+			}
+		}
+		visited[arr[y][x]-'A'] = false;
+		if (isEnd) {
+			maxCount = Math.max(maxCount, count);
+		}
+	}
+	
+	public boolean isIn(int y, int x) {
+		return y >= 0 && y < h && x >= 0 && x < w;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Main_1987 m = new Main_1987();
+		m.init();
+		System.out.println(m.maxCount);
+	}
+}
