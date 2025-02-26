c18ffb644977bd1d8e9496c9c5215ddf216cce6b
나제법 <89574219+nove1080@users.noreply.github.com>
2025-02-27 08:43:28 +0900

    4주차 문제풀이 업로드

---
+++ 4주차/기본/2606.바이러스/2606_나제법.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class 2606_나제법 {
+
+	static int computer;
+	static int network;
+
+	static ArrayList<Integer>[] adjList;
+	static boolean[] vis;
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception{
+		init();
+
+		System.out.println(bfs(0));
+	}
+
+	public static void init() throws Exception {
+		computer = Integer.parseInt(br.readLine());
+		network = Integer.parseInt(br.readLine());
+
+		vis = new boolean[computer];
+
+		adjList = new ArrayList[computer];
+		for (int i = 0; i < computer; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < network; i++) {
+			String[] input = br.readLine().split(" ");
+			int num1 = Integer.parseInt(input[0]) - 1;
+			int num2 = Integer.parseInt(input[1]) - 1;
+
+			adjList[num1].add(num2);
+			adjList[num2].add(num1);
+		}
+	}
+
+	public static int bfs(int start) {
+		int count = 0;
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		vis[start] = true;
+
+		while (!q.isEmpty()) {
+			Integer cur = q.poll();
+
+			for(Integer next : adjList[cur]) {
+				if(vis[next]) continue;
+
+				q.add(next);
+				vis[next] = true;
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
