f6bc055fb5ca6f9de5ba7f4d179b772bcdc4d5f3
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2025-03-06 17:48:22 +0900

    Update 곽민주.java

--- 5주차/기본/10819. 차이를 최대로/곽민주.java
+++ 5주차/기본/10819. 차이를 최대로/곽민주.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,17 +13,26 @@ public class Main {
     static int[] newArr;
 
     static void dfs(int depth) {
+    	//원소 N개를 모두 선택했다면
         if (depth == N) {
        //     System.out.println(Arrays.toString(newArr));
-            int sum = 0;
+            //매번 새롭게 sum을 구해야하므로 초기화
+        	int sum = 0;
+        	// 0부터 N-2까지 양옆에 있는 원소를 빼고
+        	//뺀 값을 sum 변수에 더함
             for (int i = 0; i < N - 1; i++) {
                 int subtract = Math.abs(newArr[i] - newArr[i + 1]);
                 sum += subtract;
             }
+            //더 큰 값이 들어오면 최댓값 (ans)을 갱신
             ans = Math.max(ans, sum);
             return;
         }
 
+        //원소들을 순서를 고려하여 나열 (순열)
+        //매번 구해진 배열은 newArr라는 배열에 저장
+        //재귀에서 depth 깊이가 N이라면
+        //즉 원소 N개를 모두 선택했다면 ex) 20 1 8 15 4 10 위 배열이 newArr에 저장될거임
         for (int i = 0; i < N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
@@ -43,6 +54,10 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(s[i]);
         }
+
+        // 20 1 15 8 4 10
+        //|A[0] - A[1]| + |A[1] - A[2]| + ... + |A[N-2] - A[N-1]| 의 최댓값을 구하는 것이 목표
+        // A[0]부터 A[N-1] 까지 원소들을 적절히 배치해서 최댓값을 구해야 한다.
 
         dfs(0);
 
