e41165fc96e02ae765b3ff7652df091016991244
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-13 14:03:26 +0900

    Update and rename 김소중.java to 김소중_HashMap.java

--- 6주차/기본/10816.숫자 카드 2/김소중_HashMap.java
+++ 6주차/기본/10816.숫자 카드 2/김소중_HashMap.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class 김소중 {
+public class 김소중_HashMap {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
