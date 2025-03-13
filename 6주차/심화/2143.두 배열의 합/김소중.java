ea99af2435b59b2c8c793f82981ec83ef42486e6
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-13 15:22:32 +0900

    Update 김소중.java

--- 6주차/심화/2143.두 배열의 합/김소중.java
+++ 6주차/심화/2143.두 배열의 합/김소중.java
@@ -1,50 +1,56 @@
 import java.io.*;
+import java.util.*;
 
-public class 김소중 {
-	static int N, left, right;
-	static int[] arr;
+public class Main {
+	static int T,N,M;
+	static int[] A, B;
+	static Map<Integer, Integer> mapA, mapB;
     
     public static void main(String[] args) throws IOException {
         init();
-        movePointer();
-        System.out.println(arr[left] + " " + arr[right]);
-    }
-
-	private static void movePointer() {
-		int minDiff = Integer.MAX_VALUE;
-        int pL = 0, pR = N-1;
+        mapInit(mapA, N, A);
+        mapInit(mapB, M, B);
         
-        while (pL < pR) {
-        	int diff = arr[pL]+arr[pR];
-        	
-        	if (minDiff > Math.abs(diff)) {
-        		minDiff = Math.abs(diff);
-        		left = pL;
-        		right = pR;
-        	}
-        	
-        	if (diff < 0) {
-        		pL++;
-        	} else {
-        		pR--;
-        	}
-        	
-        	if (minDiff == 0) break;
-        }
+        long cnt = 0;
+		for (int key : mapA.keySet()) {
+			if (!mapB.containsKey(T-key)) continue;
+			cnt += (long)mapA.get(key) * mapB.get(T-key);
+		}
 		
+        System.out.println(cnt);
+    }
+	
+	static void mapInit(Map<Integer, Integer> map, int size, int[] arr) {
+        for (int i = 0; i < size; i++) {
+        	int sum = 0;
+        	for (int j = i; j < size; j++) {
+				sum += arr[j];
+				map.merge(sum, 1, Integer::sum);
+			}
+        }
 	}
 
 	static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        T = Integer.parseInt(br.readLine());
         
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
         String[] strs = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-        	arr[i] = Integer.parseInt(strs[i]);
-		}
-		
+        	int num = Integer.parseInt(strs[i]);
+        	A[i] = num;
+        }
+        
+        M = Integer.parseInt(br.readLine());
+        B = new int[M];
+        strs = br.readLine().split(" ");
+        for (int i = 0; i < M; i++) {
+        	int num = Integer.parseInt(strs[i]);
+        	B[i] = num;
+        }
+		mapA = new HashMap<>();
+		mapB = new HashMap<>();
 	}
-    
 }
