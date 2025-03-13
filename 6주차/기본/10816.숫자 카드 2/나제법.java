38a268d1bcc44f8e31fd8cbb6e47dfe25897b1e5
나제법 <89574219+nove1080@users.noreply.github.com>
2025-03-13 14:14:28 +0900

    feat: 6주차 나제법

---
+++ 6주차/기본/10816.숫자 카드 2/나제법.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 나제법 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static int[] source;
+    static int[] targets;
+
+    public static void main(String[] args) throws Exception {
+        init();
+        Arrays.sort(source);
+        for (int target : targets) {
+            sb.append(search(target)).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static void init() throws Exception {
+        n = Integer.parseInt(br.readLine());
+        source = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            source[i] = Integer.parseInt(input[i]);
+        }
+
+        m = Integer.parseInt(br.readLine());
+        targets = new int[m];
+        input = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            targets[i] = Integer.parseInt(input[i]);
+        }
+    }
+
+    public static int search(int target) {
+        return upperBound(target) - lowerBound(target);
+    }
+
+    public static int lowerBound(int target) {
+        int st = 0;
+        int en = n;
+
+        while (st < en) {
+            int mid = (st + en) / 2;
+
+            if(source[mid] < target) st = mid + 1;
+            if(source[mid] >= target) en = mid;
+        }
+
+        return en;
+    }
+
+    public static int upperBound(int target) {
+        int st = 0;
+        int en = n;
+
+        while (st < en) {
+            int mid = (st + en) / 2;
+
+            if (source[mid] <= target) st = mid + 1;
+            if (source[mid] > target) en = mid;
+        }
+
+        return en;
+    }
+}
---
+++ 6주차/기본/1920.수찾기/나제법.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 나제법 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static int[] arr1, arr2;
+
+    public static void main(String[] args) throws Exception {
+        init();
+        Arrays.sort(arr1);
+//        binarySearchUsingLibrary();
+        for (int target : arr2) {
+            sb.append(binarySearch(arr1, target) >= 0 ? 1 : 0).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void init() throws Exception {
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        arr1 = new int[n];
+        input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(input[i]);
+        }
+
+        input = br.readLine().split(" ");
+        m = Integer.parseInt(input[0]);
+        arr2 = new int[m];
+        input = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            arr2[i] = Integer.parseInt(input[i]);
+        }
+    }
+
+    public static void binarySearchUsingLibrary() {
+        for (int target : arr2) {
+            sb.append(Arrays.binarySearch(arr1, target) >= 0 ? 1 : 0).append("\n");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int st = 0;
+        int en = arr.length - 1;
+
+        while (st <= en) {
+            int mid = (st + en) / 2;
+
+            if(arr[mid] < target) {
+                st = mid + 1;
+            } else if(arr[mid] > target) {
+                en = mid - 1;
+            } else { //발견
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
