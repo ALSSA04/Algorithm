0713a7476c2bda642967922c274e50fee347fd5d
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2025-03-13 14:08:42 +0900

    Update 곽민주.java

--- 6주차/기본/10816.숫자 카드 2/곽민주.java
+++ 6주차/기본/10816.숫자 카드 2/곽민주.java
@@ -1,42 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] arr;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		String[] s = br.readLine().split(" ");
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(s[i]);
-		}
-		Arrays.sort(arr);
-		M = Integer.parseInt(br.readLine());
-		s = br.readLine().split(" ");
-		A: for (int i = 0; i < M; i++) {
-			int target = Integer.parseInt(s[i]);
-			int left = 0;
-			int right = N - 1;
-			int mid = N / 2;
-			while (left <= right) {
-				mid = (left + right) / 2;
-				if (arr[mid] == target) {
-					System.out.println(1);
-					continue A;
-				}
-				else if (arr[mid] > target) {
-					right = mid - 1;
-				} else if (arr[mid] < target) {
-					left = mid + 1;
-
-				}
-			}
-			System.out.println(0);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] countList = new int[20000001];
+        int OFFSET = 10000000;
+        
+        String[] nInput = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(nInput[i]);
+            countList[num + OFFSET]++;
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        String[] mInput = br.readLine().split(" ");
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(mInput[i]);
+            sb.append(countList[num + OFFSET]).append(" ");
+        }
+        
+        System.out.println(sb.toString());
+    }
 }
