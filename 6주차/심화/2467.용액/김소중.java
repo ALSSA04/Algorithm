5e1670da18c21d759162860eb13e765b372a20f1
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-13 13:51:09 +0900

    Add files via upload

---
+++ 6주차/심화/2467.용액/김소중.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class 김소중 {
+	static int N, left, right;
+	static int[] arr;
+    
+    public static void main(String[] args) throws IOException {
+        init();
+        movePointer();
+        System.out.println(arr[left] + " " + arr[right]);
+    }
+
+	private static void movePointer() {
+		int minDiff = Integer.MAX_VALUE;
+        int pL = 0, pR = N-1;
+        
+        while (pL < pR) {
+        	int diff = arr[pL]+arr[pR];
+        	
+        	if (minDiff > Math.abs(diff)) {
+        		minDiff = Math.abs(diff);
+        		left = pL;
+        		right = pR;
+        	}
+        	
+        	if (diff < 0) {
+        		pL++;
+        	} else {
+        		pR--;
+        	}
+        	
+        	if (minDiff == 0) break;
+        }
+		
+	}
+
+	static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        
+        String[] strs = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(strs[i]);
+		}
+		
+	}
+    
+}
