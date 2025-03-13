b63196ac76f8d6550922c0bbf9eee54a789c3cda
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-13 13:47:59 +0900

    Add files via upload

---
+++ 6주차/기본/10816.숫자 카드 2/김소중_HashMap.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class 김소중_HashMap {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		HashMap<Integer, Integer> map = new HashMap<>(Integer.parseInt(br.readLine()));
+		for (String str : br.readLine().split(" ")) {
+			int i = Integer.parseInt(str);
+            map.merge(i, 1, Integer::sum);
+		}
+		
+		br.readLine();
+		for (String str : br.readLine().split(" ")) {
+			int i = Integer.parseInt(str);
+			int result = map.getOrDefault(i, 0);
+			sb.append(result+" ");
+		}
+		System.out.println(sb.toString());
+	}
+}
