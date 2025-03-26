965e6e46db675e7f7567d9f6f97d4bc7d289bbe7
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-26 11:56:52 +0900

    Add files via upload

---
+++ 8주차/백트래킹/심화/2661.좋은수열/김소중.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class 김소중 {
+	static int N;
+	static int[] nums;
+	
+	public static void main(String[] args) throws IOException {
+		init();
+		per(0);
+		print();
+	}
+
+	static boolean per(int depth) {
+		if (isBad(depth)) return false;
+		if (depth == N) {
+			return true;
+		}
+		
+		for (int i = 1; i <= 3; i++) {
+			nums[depth]=i;
+			if (per(depth+1)) return true;
+		}
+		
+		return false;
+	}
+
+	static boolean isBad(int depth) {
+		int a,b;
+		for (int i = 1; i <= depth/2; i++) {	// 부분 수열 크기
+			for (int j = 0; j <= depth-(i*2); j++) {	// 부분 수열 시작 index
+				for (int k = j; k <= depth-(i*2); k+=i) {
+					a = numsToInt(k, i-1);
+					b = numsToInt(k+i, i-1);
+					if (a==b) return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	static int numsToInt(int s, int e) {
+		int num = 0;
+		for (int i = s; i <= s+e; i++) {
+			num += nums[i];
+			if (i != s+e) num*=10;
+		}
+		return num;
+	}
+
+	static void print() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append(nums[i]);
+		}
+		System.out.println(sb);
+	}
+
+	static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N];
+	}
+}
